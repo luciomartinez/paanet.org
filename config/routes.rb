3757285dd9384c6e78cb4cb983d56f4c5fa25972
Lucio Martinez <martinezlucio.com@gmail.com>
2017-08-21 21:54:56 -0300

    #6 Create boilerplate goals view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'goals/index'
+  match "/objetivos", to: "goals#index", via: 'get'
+
   get 'contact/index'
   post "contact/index" => "contact#submit"
   match "/contacto", to: "contact#index", via: 'get'
